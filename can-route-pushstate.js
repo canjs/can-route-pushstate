1896603972ef31705635fe986ef31a4a75eeac49
Justin Meyer <justinbmeyer@gmail.com>
2018-09-24 21:47:28 -0500

    fixes bugs caused by adding .value to single value observables

--- can-route-pushstate.js
+++ can-route-pushstate.js
@@ -129,17 +129,17 @@ canReflect.assign(PushstateObservable.prototype, {
 	paramsMatcher: /^\?(?:[^=]+=[^&]*&)*[^=]+=[^&]*/,
 	querySeparator: '?',
 	dispatchHandlers: function() {
-		var old = this.value;
+		var old = this._value;
 		var queuesArgs = [];
-		this.value = getCurrentUrl();
-		if (old !== this.value) {
-			queuesArgs = [this.handlers.getNode([]), this, [this.value, old]];
+		this._value = getCurrentUrl();
+		if (old !== this._value) {
+			queuesArgs = [this.handlers.getNode([]), this, [this._value, old]];
 			//!steal-remove-start
 			if (process.env.NODE_ENV !== 'production') {
 				queuesArgs = [
-					this.handlers.getNode([]), this, [this.value, old]
+					this.handlers.getNode([]), this, [this._value, old]
 					/* jshint laxcomma: true */
-					, null, [canReflect.getName(this), "changed to", this.value, "from", old]
+					, null, [canReflect.getName(this), "changed to", this._value, "from", old]
 					/* jshint laxcomma: false */
 				];
 			}
@@ -192,7 +192,7 @@ canReflect.assign(PushstateObservable.prototype, {
 						// same page and just a different hash; see can-route-pushstate#75
 						var windowPathWithSearch = window.location.pathname + window.location.search;
 						var shouldCallPreventDefault = nodePathWithSearch !== windowPathWithSearch || node.hash === window.location.hash;
-						
+
 
 						// Test if you can preventDefault
 						// our tests can't call .click() b/c this
@@ -200,7 +200,7 @@ canReflect.assign(PushstateObservable.prototype, {
 						if (shouldCallPreventDefault && e.preventDefault) {
 							e.preventDefault();
 						}
-						
+
 						// Now update window.location
 						window.history.pushState(null, null, node.href);
 
@@ -213,7 +213,7 @@ canReflect.assign(PushstateObservable.prototype, {
 		if (isNode()) {
 			return;
 		}
-		this.value = getCurrentUrl();
+		this._value = getCurrentUrl();
 		// Intercept routable links.
 		domEvents.addDelegateListener(document.documentElement, 'click', 'a', this.anchorClickHandler);
 		var originalMethods = this.originalMethods = {};
@@ -316,7 +316,7 @@ var pushstateObservableProto = {
 //!steal-remove-start
 if (process.env.NODE_ENV !== 'production') {
 	pushstateObservableProto["can.getName"] = function() {
-			return "PushstateObservable<" + this.value + ">";
+			return "PushstateObservable<" + this._value + ">";
 	};
 }
 //!steal-remove-end
